e271d2900c8af39f657b8bbd9e5ac3a43c728a1f
JiaYu Wu <jiayu.wu666@gmail.com>
2021-10-14 15:36:22 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 
 const exphbs = require('express-handlebars')
 
+const Todo = require('./models/todo')
+
 const app = express()
 
 // setting routes
@@ -25,7 +27,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // get all the Todo data
+  Todo.find()
+    .lean()
+    .then((todos) => res.render('index', { todos }))
+    .catch((error) => console.error(error))
 })
 
 // listen
