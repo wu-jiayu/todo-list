30d0ee14417ae136d13bb38f8604acff1c3d9cdc
JiaYu Wu <jiayu.wu666@gmail.com>
2021-10-14 10:23:05 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
 // setting routes
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
   res.send('hello world!')
 })
